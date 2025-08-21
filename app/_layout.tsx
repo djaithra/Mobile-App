dfe52db0ba728080ef8c9ade1143b623935f9a33
Dharmesh Srivastava <v-dharmeshs@microsoft.com>
2025-08-21 10:45:33 +0530

    Removed Gluestack UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import "@/global.css";
 import { Stack} from "expo-router";
+import { StyleSheet } from 'react-native';
+// TODO: Implement dark mode handling using a supported method or library if needed.
 
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
+//import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 // import { config } from "@/components/ui/gluestack-ui.config";
 
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider>
-      <Stack/>
-    </GluestackUIProvider>    
+    // <GluestackUIProvider>
+    //   <Stack/>
+    // </GluestackUIProvider>   
+      <Stack/> 
   ); 
 
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import { View, Text, FlatList } from "react-native";
 // import products from "../assets/products.json";
 // import ProductListItem from "../components/ProductListItem";
-import { Button, ButtonText } from "@/components/ui/button";
+//import { Button, ButtonText } from "@/components/ui/button";
 import React = require("react");
 
 export default function HomeScreen() {
   return (
-    <Button>
-      <ButtonText>Press me</ButtonText>
-    </Button>
+    // <Button>
+    //   <ButtonText>Press me</ButtonText>
+    // </Button>
+    <Text>Hello World</Text>
   );
 }
 
