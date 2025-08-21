348fee3eba7a762c957fb97bdb335ecf0aafee61
Dharmesh Srivastava <v-dharmeshs@microsoft.com>
2025-08-21 09:52:09 +0530

    Working Version with Package issues resoved

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,16 @@
-
+import * as React from "react";
+import "@/global.css";
 import { Stack} from "expo-router";
+
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+// import { config } from "@/components/ui/gluestack-ui.config";
+
+
 export default function RootLayout() {
   return (
-    <Stack/>
-  );
+    <GluestackUIProvider>
+      <Stack/>
+    </GluestackUIProvider>    
+  ); 
+
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
-import { View,Text, FlatList } from "react-native";
-import products from "../assets/products.json";
-import ProductListItem from "../components/ProductListItem";
+import { View, Text, FlatList } from "react-native";
+// import products from "../assets/products.json";
+// import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
+import React = require("react");
 
 export default function HomeScreen() {
   return (
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-      />
+    <Button>
+      <ButtonText>Press me</ButtonText>
+    </Button>
   );
 }
+
+
