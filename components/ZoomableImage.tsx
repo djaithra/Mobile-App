2ef6181aceddab879704e1b09731545b1b696767
Dharmesh Srivastava <v-dharmeshs@microsoft.com>
2025-08-25 22:48:27 +0530

    Add the Moving behavior to the image.

--- components/ZoomableImage.tsx
+++ components/ZoomableImage.tsx
@@ -6,21 +6,45 @@ import Animated, {
   useSharedValue,
 } from "react-native-reanimated";
 
-export default function ZoomableImage({ uri }: { uri: string }) {
+type ZoomableImageProps = {
+  uri: string;
+};
+
+const ZoomableImage: React.FC<ZoomableImageProps> = ({ uri }) => {
   const scale = useSharedValue(1);
-  const focalX = useSharedValue(0);
-  const focalY = useSharedValue(0);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const lastTranslateX = useSharedValue(0);
+  const lastTranslateY = useSharedValue(0);
 
   const pinchGesture = Gesture.Pinch().onUpdate((e) => {
     scale.value = e.scale;
   });
 
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      if (scale.value > 1) {
+        translateX.value = lastTranslateX.value + e.translationX;
+        translateY.value = lastTranslateY.value + e.translationY;
+      }
+    })
+    .onEnd(() => {
+      lastTranslateX.value = translateX.value;
+      lastTranslateY.value = translateY.value;
+    });
+
+  const composed = Gesture.Simultaneous(pinchGesture, panGesture);
+
   const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ scale: scale.value }],
+    transform: [
+      { scale: scale.value },
+      { translateX: scale.value > 1 ? translateX.value : 0 },
+      { translateY: scale.value > 1 ? translateY.value : 0 },
+    ],
   }));
 
   return (
-    <GestureDetector gesture={pinchGesture}>
+    <GestureDetector gesture={composed}>
       <Animated.View style={[{ width: "100%", height: 240 }, animatedStyle]}>
         <Image
           source={{ uri }}
@@ -30,4 +54,6 @@ export default function ZoomableImage({ uri }: { uri: string }) {
       </Animated.View>
     </GestureDetector>
   );
-}
+};
+
+export default ZoomableImage;
